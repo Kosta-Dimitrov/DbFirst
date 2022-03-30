319086a2852eb06169bb7dcbd93d885fdcdbeac6
kosta_19 <kostadimitrov19@gmail.com>
2022-03-30 11:22:46 +0300

    just push something

--- DbFirst/Program.cs
+++ DbFirst/Program.cs
@@ -9,9 +9,7 @@
         {
             using (database1Context context = new database1Context())
             {
-                //Educationformtype educationformtype= new Educationformtype { Id=12, Isactive=true};
-                //CRUDOperations.AddInstitution(context, "My speciality", true, 5, 44);
-                //CRUDOperations.AddNewEducationFormType(context,"new type");
+                //comment just for git
                 CRUDOperations.AddEducation<Educationformtype>(context, "My educationform type", true, 8);
             }
         }
